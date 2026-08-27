3906440ffb497b9714feab6a94b23e5285eb94e1
Shrffln <115260881+Shrffln@users.noreply.github.com>
2026-08-27 02:56:26 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -1,5 +1,5 @@
 // ============================================
-//Trojan TRADING BOT - Complete Implementation
+// Axiom TRADING BOT - Complete Implementation
 // with Debug Logging & Professional Messages 
 // ============================================
 import { Telegraf, Markup } from 'telegraf';
